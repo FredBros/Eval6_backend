e7227ff90b61ac99ec27e0b722cd261723970de0
FredBros <77939827+FredBros@users.noreply.github.com>
2022-05-27 13:14:56 +0200

    add DB connection

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -7,13 +7,31 @@ import {Sequelize} from 'sequelize'
   // npm install mariadb --save
   // creer la DB avec XAMPP / phpMyAdmin
 
-  const sequelize = new Sequelize("eval6", "root", "", {
+  let sequelize
+
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(
+    process.env.JAWSDB_DB_NAME,
+    process.env.JAWSDB_USERNAME,
+    process.env.JAWSDB_PW,
+
+    {
+      host: process.env.JAWSDB_HOST,
+      dialect: "mariadb",
+      dialectOptions: {
+        timezone: "Etc/GMT-2",
+      },
+      logging: true,
+    }
+  );
+} else {
+   sequelize = new Sequelize("eval6", "root", "", {
     host: "localhost",
     dialect: "mariadb",
     dialectOptions: {
       timezone: "Etc/GMT-2",
     },
     logging: false,
-  });
+  })}
 
 export default sequelize
